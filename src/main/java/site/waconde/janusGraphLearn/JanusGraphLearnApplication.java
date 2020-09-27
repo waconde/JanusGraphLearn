88400eaa6de0da54fef6fab2ffb5a5c750af1511
side.wang <side.wang@percent.cn>
2020-09-27 16:35:06 +0800

    初始化spring boot项目

---
+++ src/main/java/site/waconde/janusGraphLearn/JanusGraphLearnApplication.java
@@ -0,0 +1,13 @@
+package site.waconde.janusGraphLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusGraphLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanusGraphLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/waconde/janusGraphLearn/JanusGraphLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package site.waconde.janusGraphLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanusGraphLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
